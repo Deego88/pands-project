dbf89e482b922d92392fed10f0766ae482bee8e4
Deego88 <60344243+Deego88@users.noreply.github.com>
2020-03-29 14:14:06 +0100

    Add files via upload

---
+++ analysis.py
@@ -0,0 +1,11 @@
+#Richard Deegan
+#Import Libraries 
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+#Read Iris CSV file to DataFrame
+df = pd.read_csv('iris.csv')
+
+#Seperate three types of Iris flower
+
